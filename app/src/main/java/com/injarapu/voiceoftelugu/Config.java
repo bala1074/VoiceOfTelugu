ce2f2bcbd84c0ea68cd618754e80240508cc6600
Balasatish Injarapu <injarapu@internal.synopsys.com>
2017-06-25 23:50:45 +0530

    Navbar with working feedback + Share

---
+++ app/src/main/java/com/injarapu/voiceoftelugu/Config.java
@@ -0,0 +1,9 @@
+package com.injarapu.voiceoftelugu;
+
+/**
+ * Created by injarapu on 6/25/2017.
+ */
+
+public class Config {
+    static String SHARE_MESSAGE = "VoiceOfTelugu Install from https://play.google.com/store/apps/details?id=com.injarapu.voiceoftelugu";
+}
